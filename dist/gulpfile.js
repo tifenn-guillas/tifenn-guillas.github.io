fc27387e6798707f8e5cda6163889e74e0ca574a
Tifenn Guillas <tifenn.guillas@gmail.com>
2021-12-14 17:19:02 +0100

    Upgrade fontawesome

--- dist/gulpfile.js
+++ dist/gulpfile.js
@@ -7,25 +7,6 @@ let gulp = require('gulp'),
 
 exports.default = function() {
     gulp.src([
-        './node_modules/prismjs/components/prism-core.js',
-        './node_modules/prismjs/components/prism-markup.js',
-        './node_modules/prismjs/components/prism-css.js',
-        './node_modules/prismjs/components/prism-clike.js',
-        './node_modules/prismjs/components/prism-javascript.js',
-        './node_modules/prismjs/components/prism-bash.js',
-        './node_modules/prismjs/components/prism-php.js',
-        './node_modules/prismjs/components/prism-php-extras.js',
-        './node_modules/prismjs/components/prism-docker.js',
-        './node_modules/prismjs/components/prism-yaml.js',
-        './node_modules/prismjs/components/prism-sql.js',
-        './node_modules/prismjs/components/prism-rest.js',
-        './node_modules/prismjs/components/prism-python.js',
-        './node_modules/prismjs/components/prism-nginx.js',
-        './node_modules/prismjs/components/prism-java.js',
-        './node_modules/prismjs/components/prism-git.js',
-        './node_modules/prismjs/components/prism-ini.js',
-        './node_modules/prismjs/plugins/file-highlight/prism-file-highlight.js',
-
         './node_modules/jquery/dist/jquery.min.js',
         './node_modules/clipboard/dist/clipboard.min.js',
         './node_modules/jticker/js/jquery.jticker.js',
@@ -37,18 +18,17 @@ exports.default = function() {
     .pipe(uglify())
     .pipe(gulp.dest('./'));
 
-    gulp.src(['./node_modules/font-awesome/fonts/**/*'])
+    gulp.src(['./node_modules/@fortawesome/fontawesome-free/webfonts/**/*'])
         .pipe(gulp.dest('assets/dist'));
 
     return gulp.src([
-        './node_modules/font-awesome/css/font-awesome.min.css',
+        './node_modules/@fortawesome/fontawesome-free/css/all.min.css',
         './node_modules/magnific-popup/dist/magnific-popup.css',
         './node_modules/noty/lib/noty.css',
         './node_modules/noty/lib/themes/light.css',
-        './node_modules/prismjs/themes/prism-okaidia.css',
     ])
     .pipe(concat('assets/dist/libs.css'))
-    .pipe(replace('../fonts/fontawesome-', 'fontawesome-'))
+    .pipe(replace('../webfonts/fa-', 'fa-'))
     .pipe(uglifycss())
     .pipe(gulp.dest('./'));
 }
